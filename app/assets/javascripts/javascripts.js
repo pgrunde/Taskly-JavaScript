229ca1dd81fdf1e648f48ed5906562d52b3a55e5
Peter Grunde <peter.a.grunde@gmail.com>
2014-08-12 16:40:33 -0600

    color flags for late things

--- app/assets/javascripts/javascripts.js
+++ app/assets/javascripts/javascripts.js
@@ -1,7 +1,47 @@
 $(document).ready(function(){
-  $('.task-list').on("click",function(){
-    var togglin = $(this).siblings('.tasks');
+
+
+  var stopFlash = function() {
+    $('.errors').slideUp();
+  };
+
+  window.setTimeout(stopFlash,5000);
+
+  $('.errors').click(function(){
+    $('.errors').slideUp();
+  });
+
+  $('.up-shield').on("click",function(){
+
+    $(this).hide();
+
+    $('.up-shield').not($(this)).show();
+    $('.down-shield').not($(this)).hide();
+    $(this).siblings('.down-shield').show();
+
+    var togglin = $(this).parents().parents().siblings('.tasks');
     togglin.slideToggle();
     $('.tasks').not(togglin).slideUp();
+
   });
+
+  $('.down-shield').on("click",function(){
+    var togglin = $(this).parents().parents().siblings('.tasks');
+    togglin.slideToggle();
+    $(this).hide()
+    $(this).siblings('.up-shield').show();
+  });
+
+  var dueDate = $('.due-date');
+  dueDate.each(function(){
+    var num = this.innerHTML.split(" ");
+    num = parseInt(num[0]);
+    console.log(num);
+    if(num < 0){
+      $(this).css("background-color","red");
+    } else if(num == 0){
+      $(this).css("background-color","orange");
+    }
+  });
+
 });
