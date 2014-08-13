7aaaf8b01a19c34e3032f24f42c69055907fd993
Peter Grunde <peter.a.grunde@gmail.com>
2014-08-13 16:01:57 -0600

    clear button and datepicker added, datepicker doesn't play nice with rails yet

--- app/assets/javascripts/javascripts.js
+++ app/assets/javascripts/javascripts.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 //  day one jQuery
-  var stopFlash = function() {
+  var stopFlash = function () {
     $('.errors').slideUp();
   };
 
-  window.setTimeout(stopFlash,5000);
+  window.setTimeout(stopFlash, 5000);
 
-  $('.errors').click(function(){
+  $('.errors').click(function () {
     $('.errors').slideUp();
   });
 
-  $('.up-shield').on("click",function(){
+  $('.up-shield').on("click", function () {
 
     $(this).hide();
 
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
   });
 
-  $('.down-shield').on("click",function(){
+  $('.down-shield').on("click", function () {
     var togglin = $(this).parents().parents().siblings('.tasks');
     togglin.slideToggle();
     $(this).hide()
@@ -32,30 +32,55 @@ $(document).ready(function(){
   });
 
   var dueDate = $('.due-date');
-  dueDate.each(function(){
+  dueDate.each(function () {
     var num = this.innerHTML.split(" ");
     num = parseInt(num[0]);
-    if(num < 0){
-      $(this).css("background-color","red");
-    } else if(num == 0){
-      $(this).css("background-color","orange");
+    if (num < 0) {
+      $(this).css("background-color", "red");
+    } else if (num == 0) {
+      $(this).css("background-color", "orange");
     }
   });
 
 //  day two jQuery
-  $('input[type="submit"]').attr('disabled','disabled');
-  $("input[type=text]").keyup(function(){
-    if(($(this).val()) != ""){
-      $("input[type='submit']").removeAttr('disabled')
+  $('input[type="submit"]').attr('disabled', 'disabled');
+
+  $("#new-task-page input[type=text], #new-task-page input[type=select-date]").focusout(function () {
+    if (($(this).val()) != "") {
+      $("input[type='submit']").removeAttr('disabled');
+      $(".dingus").hide();
+      $(".warning-triangle").hide();
     } else {
-      $('input[type="submit"]').attr('disabled','disabled');
+      $('input[type="submit"]').attr('disabled', 'disabled');
+      $(this).siblings('.warning-triangle').show();
+      $(".dingus").show();
     }
   });
-  $('input[type=text], textarea').keyup(function(){
-    if($(this).val() != "") {
-      $("input[type='submit']").removeAttr('disabled')
+  $('input[type=text], textarea').focusout(function () {
+    if ($(this).val() != "") {
+      $("input[type='submit']").removeAttr('disabled');
+      $(".dingus").hide();
+      $(".warning-triangle").hide();
     } else {
-      $('input[type="submit"]').attr('disabled','disabled');
+      $('input[type="submit"]').attr('disabled', 'disabled');
+      $(this).siblings('.warning-triangle').show();
+      $(".dingus").show();
     }
   });
+
+  $('input[type=submit]').on("click", function () {
+    $("dingus").empty.show.append("<h2>Submitting...</h2>");
+    $('input:submit').attr("disabled",true);
+  });
+
+  $(function() {
+    $('.select-date').datepicker();
+  });
+
+  $('.clear-button').on("click", function(){
+    $("input[type=text]").val("");
+    $("input[type=text], textarea").val("");
+    $('.select-date').val('mm/dd/yyyy')
+  });
+
 });
